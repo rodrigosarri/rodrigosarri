5b22068d99c9569a6ee6045e3820afa09aa3aa26
Rodrigo Sarri <rodrigoasarri@gmail.com>
2024-02-25 14:34:30 -0300

    feat(css-variables): adicionar variáveis CSS #9

---
+++ MyOnlineResume/frontend/react-resume/src/GlobalStyle.tsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --size-xs: 8px;
+    --size-sm: 16px;
+    --size-md: 24px;
+    --size-lg: 32px;
+    --size-xl: 40px;
+
+    --color-snowy-white: #FFF;
+    --color-froggy-green: #4CD964;
+    --color-sky-high-blue: #007AFF;
+    --color-sunny-yellow: #FC0;
+    --color-smokey-gray: #B3B3B3;
+    --color-midnight-gray: #4A4A4A;
+  }
+`;
