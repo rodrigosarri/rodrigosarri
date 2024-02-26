c4379c1535f3f2c549975d8c82c39973dd12790f
Rodrigo Sarri <rodrigoasarri@gmail.com>
2024-02-26 00:12:04 -0300

    feat(skeleton-component): refactor to support light/dark mode #37

--- MyOnlineResume/frontend/react-resume/src/components/Skeleton/index.tsx
+++ MyOnlineResume/frontend/react-resume/src/components/Skeleton/index.tsx
@@ -1,6 +1,10 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { SkeletonProps } from "./interface";
+import { ThemeState } from "src/redux/theme/interface";
+
 import { SkeletonPulse } from "./styled";
+
+import { themeStore } from "src/redux/theme";
 
 export const Skeleton: FC<SkeletonProps> = ({
   id,
@@ -10,6 +14,18 @@ export const Skeleton: FC<SkeletonProps> = ({
   height,
   borderRadius
 }) => {
+  const [theme, setTheme] = useState<ThemeState["currentTheme"]>(
+    (() => {
+      const storedTheme = localStorage.getItem("theme");
+      return storedTheme ? (storedTheme as (ThemeState["currentTheme"])) : "dark";
+    })
+  );
+
+  themeStore.subscribe(() => {
+    const newTheme = themeStore.getState();
+    setTheme(newTheme.currentTheme);
+  });
+
   return (
     <SkeletonPulse
       id={id}
@@ -18,6 +34,7 @@ export const Skeleton: FC<SkeletonProps> = ({
       width={width}
       height={height}
       borderRadius={borderRadius}
+      theme={theme}
     />
   );
 };
--- MyOnlineResume/frontend/react-resume/src/components/Skeleton/styled.tsx
+++ MyOnlineResume/frontend/react-resume/src/components/Skeleton/styled.tsx
@@ -12,9 +12,12 @@ const load = keyframes`
 
 export const SkeletonPulse = styled.div<SkeletonProps>`
   width: ${({ width }) => width || "100%"};
-  height: ${({ height }) => height || "24px"};
+  height: ${({ height }) => height || "var(--size-md)"};
   border-radius: ${({ borderRadius }) => borderRadius || "4px"};
   background-size: 800px 104px;
   animation: ${load} ${({ duration }) => duration || "2s"} infinite linear forwards;
-  background-image: linear-gradient(to right, #f0f0f0 8%, #e0e0e0 18%, #f0f0f0 33%);
+  background-image: ${({ theme }) =>
+    theme === "dark"
+      ? "linear-gradient(to right, #2a2f37 8%, #3a3f47 18%, #2a2f37 33%)"
+      : "linear-gradient(to right, #f0f0f0 8%, #e0e0e0 18%, #f0f0f0 33%)"};
 `;
